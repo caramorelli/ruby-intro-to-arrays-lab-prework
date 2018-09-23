67dd2015b9d25e4f95d9343fcec52bae3780b8fb
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 01:06:39 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,11 +1,4 @@
 def instantiate_new_array
   my_new_array = []
-  # my_new_array
-end 
-# instantiate_new_array = []
-
-def array_with_two_elements
-end 
-
-def length_of_array
+  my_two_array
 end 
